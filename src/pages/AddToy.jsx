21fc0e190fcf4c010cc106432ca13c400be28f08
ah-rifat <rifat266@gmail.com>
2023-05-19 15:33:32 +0600

    add toy form validation has been done

--- src/pages/AddToy.jsx
+++ src/pages/AddToy.jsx
@@ -1,8 +1,9 @@
 import { useForm } from "react-hook-form";
 
 const AddToy = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const onSubmit = data => console.log(data);
+
 
     return (
         <div className="w-96 mx-auto rounded-xl shadow-xl border border-violet-300 shadow-violet-300 bg-white p-4 my-10">
@@ -12,40 +13,47 @@ const AddToy = () => {
                 <div className="grid">
                     <div className="mb-6">
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image URL</label>
-                        <input type="text" {...register("imageUrl")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" />
+                        <input type="text" {...register("imageUrl", { required: true })} className={errors.imageUrl ? 'border border-red-300 rounded-lg focus:ring-red-500 focus:border-red-500 block w-full' : 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500'} />
+                        <p className="mt-1 text-red-500 font-semibold">{errors.imageUrl ? 'Image URL is required' : ''}</p>
                     </div>
                     <div className="mb-6">
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Toy Name</label>
-                        <input type="text" {...register("toyName")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" />
+                        <input type="text" {...register("toyName", { required: true })} className={errors.toyName ? 'border border-red-300 rounded-lg focus:ring-red-500 focus:border-red-500 block w-full' : 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500'} />
+                        <p className="mt-1 text-red-500 font-semibold">{errors.toyName ? 'Toy Name is required' : ''}</p>
                     </div>
                     <div className="mb-6 flex items-center gap-4">
                         <div className="w-52">
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Sub-category</label>
-                            <select {...register("subCategory")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500">
-                                <option >Choose a Category</option>
+                            <select {...register("subCategory", { required: true })} className={errors.subCategory ? 'border border-red-300 rounded-lg focus:ring-red-500 focus:border-red-500 block w-full' : 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500'}>
+                                <option value={''}>Choose a Category</option>
                                 <option value="sports car">Sports Car</option>
                                 <option value="mini fire truck">Mini Fire Truck</option>
                                 <option value="police car">Police Car</option>
                                 <option value="truck">Truck</option>
                             </select>
+                            <p className="mt-1 text-red-500 font-semibold">{errors.subCategory ? 'Sub-Category is required' : ''}</p>
                         </div>
                         <div className="w-32">
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Quantity</label>
-                            <input type="number" {...register("quantity")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" />
+                            <input type="number" {...register("quantity", { required: true })} className={errors.quantity ? 'border border-red-300 rounded-lg focus:ring-red-500 focus:border-red-500 block w-full' : 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500'} />
+                            <p className="mt-1 text-red-500 font-semibold">{errors.quantity ? 'Quantity is required' : ''}</p>
                         </div>
                     </div>
                     <div className="mb-6">
                         <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                        <textarea {...register("description")} rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"></textarea>
+                        <textarea {...register("description", { required: true })} rows="4" className={errors.description ? 'border border-red-300 rounded-lg focus:ring-red-500 focus:border-red-500 block w-full' : 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500'}></textarea>
+                        <p className="mt-1 text-red-500 font-semibold">{errors.description ? 'Description is required' : ''}</p>
                     </div>
                     <div className="mb-6 flex gap-4 items-center">
                         <div className="w-1/2">
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Toy Price</label>
-                            <input type="number" {...register("price")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" />
+                            <input type="number" {...register("price", { required: true })} className={errors.price ? 'border border-red-300 rounded-lg focus:ring-red-500 focus:border-red-500 block w-full' : 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500'} />
+                            <p className="mt-1 text-red-500 font-semibold">{errors.price ? 'Price is required' : ''}</p>
                         </div>
                         <div className="w-1/2">
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Toy Rating</label>
-                            <input type="number" {...register("rating")} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" />
+                            <input type="number" {...register("rating", { required: true })} className={errors.rating ? 'border border-red-300 rounded-lg focus:ring-red-500 focus:border-red-500 block w-full' : 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-violet-500 focus:border-violet-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500'} />
+                            <p className="mt-1 text-red-500 font-semibold">{errors.rating ? 'Rating is required' : ''}</p>
                         </div>
                     </div>
                 </div>
