eb3baf026e59ee3e10c5fe295fa1a46320bf6fb2
ah-rifat <rifat266@gmail.com>
2023-05-20 17:26:35 +0600

    all toys show data in limit funtionality done

--- src/pages/AllToys.jsx
+++ src/pages/AllToys.jsx
@@ -4,19 +4,44 @@ import { Link } from "react-router-dom";
 
 const AllToys = () => {
     const [allToysData, setAllToysData] = useState([])
+    const [selectLimit, setSelectLimit] = useState(20)
 
     useEffect(() => {
-        fetch('http://localhost:5000/allToys')
+        fetch(`http://localhost:5000/allToys?limit=${selectLimit}`)
             .then(res => res.json())
             .then(result => {
                 setAllToysData(result);
             })
-    }, [allToysData])
+    }, [allToysData, selectLimit])
 
     return (
         <>
             <p className="text-center mt-4 text-xl font-serif text-violet-600 underline">All Toys</p>
-            <div className="w-10/12 mx-auto my-10 shadow-violet-300 border border-violet-300 relative overflow-x-auto shadow-md sm:rounded-lg">
+
+            <div className="w-[83%] ms-28  flex items-center justify-between mb-6">
+                <div className="flex items-center">
+                    <label className="block w-56 mb-2 text-md font-medium text-gray-900 dark:text-white">Limit Per Page</label>
+                    <select onChange={(e) => setSelectLimit(e.target.value)} className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+
+                        <option value={2}>2</option>
+                        <option value={20}>20</option>
+                        <option value={50}>50</option>
+                        <option value={100}>100</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label htmlFor="table-search" className="sr-only">Search</label>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
+                        </div>
+                        <input type="text" id="table-search-users" className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search on Toy Name" />
+                    </div>
+                </div>
+            </div>
+
+            <div className="w-10/12 mx-auto mb-10 shadow-violet-300 border border-violet-300 relative overflow-x-auto shadow-md sm:rounded-lg">
                 <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-violet-100 dark:bg-gray-700 dark:text-gray-400">
                         <tr>
